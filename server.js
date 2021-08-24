b202eb7c65f95a537e793e365407ea29c3855ef7
Enrico <enrico.curiotto@bonitasoft.com>
2021-08-24 15:46:36 +0200

    updated dependencies

--- server.js
+++ server.js
@@ -9,7 +9,7 @@
 
     // configuration =================
 
-    mongoose.connect('mongodb://localhost/montagna');     // connect to mongoDB database locally
+    mongoose.connect('mongodb://localhost/montagna', {useNewUrlParser: true, useUnifiedTopology: true});     // connect to mongoDB database locally
 
     app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
     app.use(morgan('dev'));                                         // log every request to the console
@@ -160,7 +160,7 @@
 
     // application -------------------------------------------------------------
     app.get('*', function(req, res) {
-        res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+        res.sendFile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
     });
 	
 	var today  = new Date();
